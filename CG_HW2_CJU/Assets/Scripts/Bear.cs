9a979b5d09b006e77fdcd8125d1a0da8b364c0fc
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2022-11-24 02:17:18 +0900

    곰 몬스터 AI 수정

--- CG_HW2_CJU/Assets/Scripts/Bear.cs
+++ CG_HW2_CJU/Assets/Scripts/Bear.cs
@@ -16,7 +16,6 @@ public class Bear : MonoBehaviour
     enum State
     { 
         Idle,
-        Walk,
         Run,
         Attack
     }
@@ -31,7 +30,7 @@ public class Bear : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
 
-        StartCoroutine(move());
+        StartCoroutine("move");
     }
 
     // Update is called once per frame
@@ -56,27 +55,31 @@ public class Bear : MonoBehaviour
     private void UpdateAttack()
     {
         agent.speed = 0;
-
+        anim.SetBool("Run Forward", false);
         float distance = Vector3.Distance(transform.position, player.transform.position);
 
-        if(distance > 2 && distance < 7)
+        if(distance > 2 && distance <= 7)
         {
+
             state = State.Run;
 
             anim.SetBool("Run Forward", true);
+        }
+        else if(distance > 7)
+        {
+            state = State.Idle;
+            anim.SetBool("Run Forward", false);
         }
     }
 
     private void UpdateRun()
     {
-
-
         //남은 거리가 2미터라면 공격한다.
         float distance = Vector3.Distance(transform.position, player.transform.position);
         if (distance <= 2)
         {
             state = State.Attack;
-            anim.SetTrigger("Attack");
+            anim.SetTrigger("Attack1");
         }
 
         //타겟 방향으로 이동하다가
@@ -90,13 +93,16 @@ public class Bear : MonoBehaviour
     {
         agent.speed = 0;
 
+        
+
         float distance = Vector3.Distance(transform.position, player.transform.position);
         //생성될때 목적지(Player)를 찿는다.
         //target을 찾으면 Run상태로 전이하고 싶다.
-        if (distance <= 5)
+        if (distance <= 7)
         {
+            StopCoroutine("move");
             state = State.Run;
-            //이렇게 state값을 바꿨다고 animation까지 바뀔까? no! 동기화를 해줘야한다.
+
             anim.SetBool("Run Forward", true);
         }
         else
@@ -110,19 +116,22 @@ public class Bear : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
 
-        float dir1 = Random.Range(-1f, 1f);
-        float dir2 = Random.Range(-1f, 1f);
+        while(true)
+        {
+            float dir1 = Random.Range(-1f, 1f);
+            float dir2 = Random.Range(-1f, 1f);
 
-        rigid.velocity = new Vector3(dir1, 0, dir2);
+            yield return new WaitForSeconds(5f);
 
-        transform.LookAt(transform.position + rigid.velocity * Time.deltaTime);
+            rigid.velocity = new Vector3(dir1, 0, dir2);
 
-        Quaternion newRotation = Quaternion.LookRotation(transform.position + rigid.velocity);
+            transform.LookAt(transform.position + rigid.velocity * Time.deltaTime);
 
-        rigid.rotation = Quaternion.Slerp(rigid.rotation, newRotation, 100f * Time.deltaTime);
+            Quaternion newRotation = Quaternion.LookRotation(transform.position + rigid.velocity);
 
-        anim.SetBool("WalkForward", rigid.velocity != Vector3.zero);
+            rigid.rotation = Quaternion.Slerp(rigid.rotation, newRotation, 100f * Time.deltaTime);
 
-        yield return new WaitForSeconds(5f);
+            anim.SetBool("WalkForward", rigid.velocity != Vector3.zero);
+        }
     }
 }
